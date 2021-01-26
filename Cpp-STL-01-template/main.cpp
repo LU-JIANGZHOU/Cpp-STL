dfbd480a216e9d9e01ba59dc76d57b9718bd611a
LU-JIANGZHOU <jiangzhoulu@outlook.com>
2021-01-26 23:21:55 +0800

    add template function

--- Cpp-STL-01-template/main.cpp
+++ Cpp-STL-01-template/main.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 using namespace std;
+// template function 
+template <class T> 
+T myMax(T x, T y) 
+{ 
+   return (x > y)? x: y; 
+} 
 
 int main()
 {
-  cout<<"demonstrate how template works"<<endl;
-  
+  cout<<"Demonstrate how template functions works"<<endl;
+  cout << myMax<int>(2, 5) << endl;  //for int 
+  cout << myMax<double>(1.0, 9.0) << endl; //for double 
+  cout << myMax<string>("habchll", "defgkkhkg") << endl;   //for string, only compare the first character
+   
+
   return EXIT_SUCCESS;
 }
 ;
