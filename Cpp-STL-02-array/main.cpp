199646330ae1de7f923fed5f9a5591cca1c49e05
LU-JIANGZHOU <jiangzhoulu@outlook.com>
2021-01-30 20:43:29 +0800

    update array

--- Cpp-STL-02-array/main.cpp
+++ Cpp-STL-02-array/main.cpp
@@ -9,6 +9,32 @@ using namespace std;
 int main()
 {
   cout << "to demonstrate stl container array" << endl;
+
+  array<int, 5> a = {1, 5, 2, 4, 3};
+  cout << "\nSize of array is " << a.size() << endl;
+  auto pos = a.begin();
+  cout << endl;
+  while (pos != a.end())
+    cout << *pos++ << "\t";
+  cout << endl;
+
+  array<int, 6> b;
+  for (int j = 0; j<b.size(); j++)
+    b[j] = j + 100;
+
+  cout << "\nPrint values in original order ..." << endl;
+
+  for (int j : b)
+    cout << j << "\t";
+  cout << endl;
+
+  cout << "\nPrint values in reverse order ..." << endl;
+
+  auto rpos = a.rbegin();
+  while (rpos != a.rend())
+    cout << *rpos++ << "\t";
+  cout << endl;
+
   //initialization
   array<int, 5> ar1{{50, 4, 1, 1, 2}};
   array<int, 5> ar2 = {1, 20, 30, 4, 5};
