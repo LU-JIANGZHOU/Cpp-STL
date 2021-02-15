41deaffe8b843f32804bc0d5468de7aec065f25a
LU-JIANGZHOU <jiangzhoulu@outlook.com>
2021-02-15 22:59:16 +0800

    Feb 15

--- Cpp-STL-15-iterator/main.cpp
+++ Cpp-STL-15-iterator/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <set>
 
 using namespace std;
 
@@ -15,6 +16,30 @@ int main()
   for (it = iBegin; it < iEnd; it++)
   {
     cout << *it << endl;
+  }
+  cout << endl;
+
+  set<int> s1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  set<int>::iterator it1; // iterator object
+
+  // iterate forward
+  for (it1 = s1.begin(); it1 != s1.end(); ++it1)
+  {
+    cout << *it1 << " ";
+  }
+  cout << endl;
+
+  // iterate backward
+  for (it1 = s1.end(); it1 != s1.begin();)
+  {
+    cout << *--it1 << " ";
+  }
+  cout << endl;
+
+  // range-based for loop
+  for (int i : s1)
+  {
+    cout << i << " ";
   }
   cout << endl;
 
